a1d1ceda0cb73cdbb50b0419c93ce5621ff5a7c5
Rohit <rohit.sharma@wisc.edu>
2020-02-24 10:32:14 -0600

    Refactored the code and added README

---
+++ assignments/assignment2/README.md
@@ -0,0 +1,29 @@
+# CS726 - HW2
+
+## Comparison of Optimization Methods for an Unconstrained Quadratic Minimization
+
+### Usage
+
+Run the following in the current directory in a MatLab shell:
+
+```MatLab
+optimization(n, iter)
+```
+
+where the dimension, `n = 200` and `iter = 850`.
+
+### Results
+
+When run with the above arguments, the code produces the three plots below:
+
+#### Part (i)
+
+![Part (i)](plot(i).png?raw=true "Part (i)")
+
+#### Part (ii)
+
+![Part (ii)](plot(ii).png?raw=true "Part (ii)")
+
+#### Part (iii)
+
+![Part (iii)](plot(iii).png?raw=true "Part (iii)")
--- assignments/assignment2/optimization.m
+++ assignments/assignment2/optimization.m
@@ -1,98 +1,103 @@
-function optimization
-    n = 200;
+function optimization(n, iter)
     [M, b] = initializeMatrix(n);
     
+    % Get L, f(x*)
     evals = sort(eig(M));
     L = evals(n);       % We can also prove L = 4 in this case
+    f_optimal = minimize(M, b);
     
-    x_optimal = pinv(M) * b;
-%     disp('Global min:');
-    f_optimal = (1/2) * dot(M*x_optimal, x_optimal) - dot(b, x_optimal);
-%     disp(f_optimal);
+    % SD:Const initializations
+    sdconst_x_k = zeros(n, 1);
+    sdconst_opt_gap = [];
     
-    iter = 850;
+    % SD:Exact initializations
+    sdexact_x_k = zeros(n, 1);
+    sdexact_opt_gap = [];
     
-    steep_desc_x_k = zeros(n, 1);
-    steep_desc_opt_gap = [];
-    
-    line_search_x_k = zeros(n, 1);
-    line_search_opt_gap = [];
-    
-    lagged_line_search_x_k = zeros(n, 1);
+    % SD:Lagged initializations
+    sdlagged_x_k = zeros(n, 1);
     step_k = 1 / L;     % initialization of prev step size for lagged line search
-    lagged_line_search_opt_gap = [];
-    lagged_line_search_lowest_f = [];
+    sdlagged_opt_gap = [];
+    sdlagged_lowest_f = [];
     
     % Nesterov's Acceleration initializations:
-    nest_x_k = zeros(n, 1);
-    a_k = 1;
+    nesterov_x_k = zeros(n, 1);
     A_k = 1;
-    v_k = nest_x_k - gradient(M, b, nest_x_k) / L;
+    v_k = nesterov_x_k - gradient(M, b, nesterov_x_k) / L;
     y_k = v_k;
-    nest_opt_gap = [];
-    nest_lowest_f = [];
+    nesterov_opt_gap = [];
+    nesterov_lowest_f = [];
     
+    % Optimization loop
     for k = 1 : iter
-        steep_desc_x_k = steepestDescent(steep_desc_x_k, M, b, L);
-        steep_desc_f = evaluate_func(M, b, steep_desc_x_k);
-        steep_desc_opt_gap = [steep_desc_opt_gap, steep_desc_f - f_optimal];
+        % Run SD:Const step
+        sdconst_x_k = steepestDescent(sdconst_x_k, M, b, L);
+        steep_desc_f = evaluate_func(M, b, sdconst_x_k);
+        sdconst_opt_gap = [sdconst_opt_gap, steep_desc_f - f_optimal];
         
-        line_search_x_k = steepestDescentLineSearch(line_search_x_k, M, b);
-        line_search_f = evaluate_func(M, b, line_search_x_k);
-        line_search_opt_gap = [line_search_opt_gap, line_search_f - f_optimal];
+        % Run SD:Exact step
+        sdexact_x_k = steepestDescentLineSearch(sdexact_x_k, M, b);
+        line_search_f = evaluate_func(M, b, sdexact_x_k);
+        sdexact_opt_gap = [sdexact_opt_gap, line_search_f - f_optimal];
         
-        [lagged_line_search_x_k, step_k] = laggedSteepestDescent(lagged_line_search_x_k, M, b, step_k);
-        lagged_line_search_f = evaluate_func(M, b, lagged_line_search_x_k);
-        lagged_line_search_opt_gap = [lagged_line_search_opt_gap, lagged_line_search_f - f_optimal];
-        if isempty(lagged_line_search_lowest_f)
-            lagged_line_search_lowest_f = lagged_line_search_f;
+        % Run SD:Lagged step
+        [sdlagged_x_k, step_k] = laggedSteepestDescent(sdlagged_x_k, M, b, step_k);
+        lagged_line_search_f = evaluate_func(M, b, sdlagged_x_k);
+        sdlagged_opt_gap = [sdlagged_opt_gap, lagged_line_search_f - f_optimal];
+        if isempty(sdlagged_lowest_f)
+            sdlagged_lowest_f = lagged_line_search_f;
         else
-            lagged_line_search_lowest_f = [lagged_line_search_lowest_f, min(lagged_line_search_lowest_f(end), lagged_line_search_f)];
+            sdlagged_lowest_f = [sdlagged_lowest_f, min(sdlagged_lowest_f(end), lagged_line_search_f)];
         end
         
-        [nest_x_k, y_k, v_k, a_k, A_k] = nesterovsMethod(nest_x_k, y_k, v_k, M, b, L, a_k, A_k);
-        nest_f = evaluate_func(M, b, y_k);
-        nest_opt_gap = [nest_opt_gap, nest_f - f_optimal];
-        if isempty(nest_lowest_f)
-            nest_lowest_f = nest_f;
+        % Run Nesterov's acceleration step
+        [y_k, v_k, A_k] = nesterovsMethod(y_k, v_k, M, b, L, A_k);
+        nesterov_f = evaluate_func(M, b, y_k);
+        nesterov_opt_gap = [nesterov_opt_gap, nesterov_f - f_optimal];
+        if isempty(nesterov_lowest_f)
+            nesterov_lowest_f = nesterov_f;
         else
-            nest_lowest_f = [nest_lowest_f, min(nest_lowest_f(end), nest_f)];
+            nesterov_lowest_f = [nesterov_lowest_f, min(nesterov_lowest_f(end), nesterov_f)];
         end
     end
     
+    % Plot part (i): Optimality gap for SD:Const, SD:Exact and Nesterov
     figure
-    plot(1:1:iter, steep_desc_opt_gap)
+    plot(1:1:iter, sdconst_opt_gap)
     set(gca, 'YScale', 'log')
     hold on
-    plot(1:1:iter, line_search_opt_gap)
+    plot(1:1:iter, sdexact_opt_gap)
     hold on
-    plot(1:1:iter, nest_opt_gap)
+    plot(1:1:iter, nesterov_opt_gap)
     legend('SD:constant', 'SD:exact', 'Nesterov')
     title('Analysis of Optimization algorithms')
     xlabel('Num iterations')
     ylabel('Optimality gap: f(x) - f(x*)');
     
+    % Plot part (ii): Optimality gap for Nesterov, SD:Lagged
     figure
-    plot(1:1:iter, nest_opt_gap)
+    plot(1:1:iter, nesterov_opt_gap)
     set(gca, 'YScale', 'log')
     hold on
-    plot(1:1:iter, lagged_line_search_opt_gap)
+    plot(1:1:iter, sdlagged_opt_gap)
     legend('Nesterov', 'SD:lagged')
     title('Analysis of Optimization algorithms')
     xlabel('Num iterations')
     ylabel('Optimality gap: f(x) - f(x*)');
     
+    % Plot part (iii): Lowest function value for Nesterov, SD:Lagged
     figure
-    plot(1:1:iter, nest_lowest_f)
+    plot(1:1:iter, nesterov_lowest_f)
     set(gca, 'YScale', 'log')
     hold on
-    plot(1:1:iter, lagged_line_search_lowest_f)
+    plot(1:1:iter, sdlagged_lowest_f)
     legend('Nesterov', 'SD:lagged')
     title('Analysis of Optimization algorithms')
     xlabel('Num iterations')
     ylabel('Lowest f value attained');
 end
 
+% Initialize M(nxn) and b(nx1) as required
 function [M, b] = initializeMatrix(n)
     k = n;
     M = diag(2*[ones(k, 1); zeros(n-k, 1)], 0)...
@@ -104,37 +109,51 @@ function [M, b] = initializeMatrix(n)
     b(1) = b(1) + 1;
 end
 
-function x_k1 = steepestDescent(x_k, M, b, L)
-    % Converges in 50000 iters to -8.3331
-    x_k1 = x_k - (1 / L) * gradient(M, b, x_k);
+% Run one step of steepest descent with constant step size = 1/L
+function x_k = steepestDescent(x_k_prev, M, b, L)
+    x_k = x_k_prev - (1 / L) * gradient(M, b, x_k_prev);
 end
 
-function x_kl = steepestDescentLineSearch(x_k, M, b)
-    desc_dirxn = -gradient(M, b, x_k);
-    step_size = dot(desc_dirxn, desc_dirxn) / dot(M*desc_dirxn, desc_dirxn);
-    x_kl = x_k + step_size * desc_dirxn;
-end
-
-function [x_k, step_kl] = laggedSteepestDescent(x_k_prev, M, b, step_kl_prev)
+% Run one step of steepest descent with exact line search
+function x_k = steepestDescentLineSearch(x_k_prev, M, b)
     desc_dirxn = -gradient(M, b, x_k_prev);
-    step_kl = dot(desc_dirxn, desc_dirxn) / dot(M*desc_dirxn, desc_dirxn);
-    x_k = x_k_prev + step_kl_prev * desc_dirxn;
+    % Compute the step size using exact line search
+    step_size = dot(desc_dirxn, desc_dirxn) / dot(M*desc_dirxn, desc_dirxn);
+    x_k = x_k_prev + step_size * desc_dirxn;
 end
 
-function [x_k, y_k, v_k, a_k, A_k] = nesterovsMethod(x_k_prev, y_k_prev, v_k_prev, M, b, L, a_k_prev, A_k_prev)
+% Run one step of steepest descent with a lagged step size
+function [x_k, step_k] = laggedSteepestDescent(x_k_prev, M, b, step_k_prev)
+    desc_dirxn = -gradient(M, b, x_k_prev);
+    % Compute new step size
+    step_k = dot(desc_dirxn, desc_dirxn) / dot(M*desc_dirxn, desc_dirxn);
+    % Use old step size to update x as per steepest descent
+    x_k = x_k_prev + step_k_prev * desc_dirxn;
+end
+
+% Run one step of Nesterov's acceleration
+function [y_k, v_k, A_k] = nesterovsMethod(y_k_prev, v_k_prev, M, b, L, A_k_prev)
     a_k = (1 + sqrt(1 + 4 * A_k_prev)) / 2;
     A_k = A_k_prev + a_k;
     
     x_k = (A_k_prev / A_k) * y_k_prev + (a_k / A_k) * v_k_prev;
     grad_x_k = gradient(M, b, x_k);
-    y_k = x_k - grad_x_k / L;
     v_k = v_k_prev - (a_k / L) * grad_x_k;
+    y_k = x_k - grad_x_k / L;
 end
 
+% Helper method to evaluate the value of function at a given input
 function f_val = evaluate_func(M, b, x)
     f_val = (1/2) * dot(M*x, x) - dot(b, x);
 end
 
+% Helper method to evaluate the gradient of function at a given input
 function grad = gradient(M, b, x)
     grad = M * x - b;
+end
+
+% Helper method to obtain the optimal value of the function (f(x*))
+function f_optimal = minimize(M, b)
+    x_optimal = pinv(M) * b;
+    f_optimal = (1/2) * dot(M*x_optimal, x_optimal) - dot(b, x_optimal);
 end
