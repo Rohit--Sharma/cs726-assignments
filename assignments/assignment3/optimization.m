325d1bf37b39587932351a3e0f4ba25e87f00d2d
Rohit <rohit.sharma@wisc.edu>
2020-03-07 00:41:22 -0600

    Completed coding, need to fix bug in str convex Nesterov Implementation

--- assignments/assignment3/optimization.m
+++ assignments/assignment3/optimization.m
@@ -2,7 +2,7 @@
 %   HW3
 %   Author: Rohit Sharma (rohit.sharma@wisc.edu)
 
-function optimization(n, iter)
+function optimization(n, m, iter)
 % OPTIMIZATION: Runs Steepest Descent variants (const step size,
 %   exact line search, lagged step size) and Nesterov's method  
 %   on a quadratic function defined by M of dimension nxn and b 
@@ -13,97 +13,97 @@ function optimization(n, iter)
     % Get L, f(x*)
     evals = sort(eig(M));
     L = evals(n);       % We can also prove L = 4 in this case
-    f_optimal = minimize(M, b);
+    f_optimal = minimize(M, b, m);
     
-    % SD:Const initializations
-    sdconst_x_k = zeros(n, 1);
-    sdconst_opt_gap = [];
-    
-    % SD:Exact initializations
-    sdexact_x_k = zeros(n, 1);
-    sdexact_opt_gap = [];
-    
-    % SD:Lagged initializations
-    sdlagged_x_k = zeros(n, 1);
-    step_k = 1 / L;     % initialization of prev step size for lagged line search
-    sdlagged_opt_gap = [];
-    sdlagged_lowest_f = [];
-    
-    % Nesterov's Acceleration initializations:
+    % Nesterov's Acceleration for smooth initializations:
     nesterov_x_k = zeros(n, 1);
-    A_k = 1;
-    v_k = nesterov_x_k - gradient(M, b, nesterov_x_k) / L;
-    y_k = v_k;
+    nesterov_A_k = 1;
+    nesterov_v_k = nesterov_x_k - gradient(M, b, m, nesterov_x_k) / L;
+    nesterov_y_k = nesterov_v_k;
     nesterov_opt_gap = [];
-    nesterov_lowest_f = [];
+    
+    % Nesterov's Acceleration for smooth monotonically decr initializations:
+    nestmono_x_k = zeros(n, 1);
+    nestmono_A_k = 1;
+    nestmono_v_k = nestmono_x_k - gradient(M, b, m, nestmono_x_k) / L;
+    nestmono_y_k = nestmono_v_k;
+    nestmono_opt_gap = [];
+    
+    % Nesterov's Acceleration for smooth and strongly convex initializations:
+    strong_nesterov_x_k = zeros(n, 1);
+    strong_nesterov_A_k = 1;
+    strong_nesterov_v_k = strong_nesterov_x_k - gradient(M, b, m, strong_nesterov_x_k) / L;
+    strong_nesterov_y_k = strong_nesterov_v_k;
+    strong_nesterov_opt_gap = [];
+    
+    % Conjugate Gradients initializations:
+    cgm_x_k = zeros(n, 1);
+    p_k = gradient(M, b, m, cgm_x_k);
+    cgm_opt_gap = [];
+    
+    % Heavy Ball initializations:
+    hbm_x_k_prev = zeros(n, 1);
+    hbm_x_k = zeros(n, 1);
+    hbm_opt_gap = [];
     
     % Optimization loop
     for k = 1 : iter
-        % Run SD:Const step
-        sdconst_x_k = steepestDescent(sdconst_x_k, M, b, L);
-        steep_desc_f = evaluate_func(M, b, sdconst_x_k);
-        sdconst_opt_gap = [sdconst_opt_gap, steep_desc_f - f_optimal];
-        
-        % Run SD:Exact step
-        sdexact_x_k = steepestDescentLineSearch(sdexact_x_k, M, b);
-        line_search_f = evaluate_func(M, b, sdexact_x_k);
-        sdexact_opt_gap = [sdexact_opt_gap, line_search_f - f_optimal];
-        
-        % Run SD:Lagged step
-        [sdlagged_x_k, step_k] = laggedSteepestDescent(sdlagged_x_k, M, b, step_k);
-        lagged_line_search_f = evaluate_func(M, b, sdlagged_x_k);
-        sdlagged_opt_gap = [sdlagged_opt_gap, lagged_line_search_f - f_optimal];
-        if isempty(sdlagged_lowest_f)
-            sdlagged_lowest_f = lagged_line_search_f;
-        else
-            sdlagged_lowest_f = [sdlagged_lowest_f, min(sdlagged_lowest_f(end), lagged_line_search_f)];
-        end
-        
         % Run Nesterov's acceleration step
-        [y_k, v_k, A_k] = nesterovsMethod(y_k, v_k, M, b, L, A_k);
-        nesterov_f = evaluate_func(M, b, y_k);
+        [nesterov_y_k, nesterov_v_k, nesterov_A_k] = nesterovsMethod(nesterov_y_k, nesterov_v_k, M, b, m, L, nesterov_A_k);
+        nesterov_f = evaluate_func(M, b, m, nesterov_y_k);
         nesterov_opt_gap = [nesterov_opt_gap, nesterov_f - f_optimal];
-        if isempty(nesterov_lowest_f)
-            nesterov_lowest_f = nesterov_f;
-        else
-            nesterov_lowest_f = [nesterov_lowest_f, min(nesterov_lowest_f(end), nesterov_f)];
-        end
+        
+        % Run Nesterov's acceleration monotonically decreasing step
+        [nestmono_y_k, nestmono_v_k, nestmono_A_k] = monotonicallyDecreasingNesterovsMethod(nestmono_y_k, nestmono_v_k, M, b, m, L, nestmono_A_k);
+        nestmono_f = evaluate_func(M, b, m, nestmono_y_k);
+        nestmono_opt_gap = [nestmono_opt_gap, nestmono_f - f_optimal];
+        
+        % Run Nesterov's acceleration for strong cvx step
+        [strong_nesterov_y_k, strong_nesterov_v_k, strong_nesterov_A_k] = nesterovForSmoothStronglyConvexRechtMethod(strong_nesterov_y_k, strong_nesterov_v_k, M, b, L, m, strong_nesterov_A_k);
+        strong_nesterov_f = evaluate_func(M, b, m, strong_nesterov_y_k);
+        strong_nesterov_opt_gap = [strong_nesterov_opt_gap, strong_nesterov_f - f_optimal];
+        
+        % Run CGM step
+        [cgm_x_k, p_k] = conjugateGradientMethod(M, b, m, cgm_x_k, p_k);
+        cgm_f = evaluate_func(M, b, m, cgm_x_k);
+        cgm_opt_gap = [cgm_opt_gap, cgm_f - f_optimal];
+        
+        % Run Heavy Ball step
+        temp = hbm_x_k;
+        hbm_x_k = heavyBallMethod(hbm_x_k, hbm_x_k_prev, M, b, L, m);
+        hbm_x_k_prev = temp;
+        hbm_f = evaluate_func(M, b, m, hbm_x_k);
+        hbm_opt_gap = [hbm_opt_gap, hbm_f - f_optimal];
     end
     
     % Plot part (i): Optimality gap for SD:Const, SD:Exact and Nesterov
     figure
-    plot(1:1:iter, sdconst_opt_gap)
-    set(gca, 'YScale', 'log')
-    hold on
-    plot(1:1:iter, sdexact_opt_gap)
-    hold on
     plot(1:1:iter, nesterov_opt_gap)
-    legend('SD:constant', 'SD:exact', 'Nesterov')
-    title('Analysis of Optimization algorithms')
+    set(gca, 'YScale', 'log')
+%     hold on
+%     plot(1:1:iter, strong_nesterov_opt_gap)
+    hold on
+    plot(1:1:iter, cgm_opt_gap)
+    hold on
+    plot(1:1:iter, hbm_opt_gap)
+    legend('Nesterov', 'Str:Nesterov', 'CGM', 'HBM')
+    title(strcat('Analysis of Optimization algorithms, m=', num2str(m)))
     xlabel('Num iterations')
     ylabel('Optimality gap: f(x) - f(x*)');
     
-    % Plot part (ii): Optimality gap for Nesterov, SD:Lagged
     figure
-    plot(1:1:iter, nesterov_opt_gap)
+    plot(1:1:iter, nestmono_opt_gap)
     set(gca, 'YScale', 'log')
+%     hold on
+%     plot(1:1:iter, strong_nesterov_opt_gap)
     hold on
-    plot(1:1:iter, sdlagged_opt_gap)
-    legend('Nesterov', 'SD:lagged')
-    title('Analysis of Optimization algorithms')
+    plot(1:1:iter, cgm_opt_gap)
+    hold on
+    plot(1:1:iter, hbm_opt_gap)
+    legend('Mono:Nesterov', 'Str:Nesterov', 'CGM', 'HBM')
+    title(strcat('Analysis of Optimization algorithms, m=', num2str(m)))
     xlabel('Num iterations')
     ylabel('Optimality gap: f(x) - f(x*)');
-    
-    % Plot part (iii): Lowest function value for Nesterov, SD:Lagged
-    figure
-    plot(1:1:iter, nesterov_lowest_f)
-    set(gca, 'YScale', 'log')
-    hold on
-    plot(1:1:iter, sdlagged_lowest_f)
-    legend('Nesterov', 'SD:lagged')
-    title('Analysis of Optimization algorithms')
-    xlabel('Num iterations')
-    ylabel('Lowest f value attained');
 end
 
 % Initialize M(nxn) and b(nx1) as required
@@ -116,37 +116,30 @@ function [M, b] = initializeMatrix(n)
     b(1) = b(1) + 1;
 end
 
-% % Run one step of steepest descent with constant step size = 1/L
-% function x_k = steepestDescent(x_k_prev, M, b, L)
-%     x_k = x_k_prev - (1 / L) * gradient(M, b, x_k_prev);
-% end
-% 
-% % Run one step of steepest descent with exact line search
-% function x_k = steepestDescentLineSearch(x_k_prev, M, b)
-%     desc_dirxn = -gradient(M, b, x_k_prev);
-%     % Compute the step size using exact line search
-%     step_size = dot(desc_dirxn, desc_dirxn) / dot(M*desc_dirxn, desc_dirxn);
-%     x_k = x_k_prev + step_size * desc_dirxn;
-% end
-% 
-% % Run one step of steepest descent with a lagged step size
-% function [x_k, step_k] = laggedSteepestDescent(x_k_prev, M, b, step_k_prev)
-%     desc_dirxn = -gradient(M, b, x_k_prev);
-%     % Compute new step size
-%     step_k = dot(desc_dirxn, desc_dirxn) / dot(M*desc_dirxn, desc_dirxn);
-%     % Use old step size to update x as per steepest descent
-%     x_k = x_k_prev + step_k_prev * desc_dirxn;
-% end
-
 % Run one step of Nesterov's acceleration
-function [y_k, v_k, A_k] = nesterovsMethod(y_k_prev, v_k_prev, M, b, L, A_k_prev)
+function [y_k, v_k, A_k] = nesterovsMethod(y_k_prev, v_k_prev, M, b, m, L, A_k_prev)
     a_k = (1 + sqrt(1 + 4 * A_k_prev)) / 2;
     A_k = A_k_prev + a_k;
     
     x_k = (A_k_prev / A_k) * y_k_prev + (a_k / A_k) * v_k_prev;
-    grad_x_k = gradient(M, b, x_k);
+    grad_x_k = gradient(M, b, m, x_k);
     v_k = v_k_prev - (a_k / L) * grad_x_k;
     y_k = x_k - grad_x_k / L;
+end
+
+function [y_k, v_k, A_k] = monotonicallyDecreasingNesterovsMethod(y_k_prev, v_k_prev, M, b, m, L, A_k_prev)
+    a_k = (1 + sqrt(1 + 4 * A_k_prev)) / 2;
+    A_k = A_k_prev + a_k;
+    
+    x_k = (A_k_prev / A_k) * y_k_prev + (a_k / A_k) * v_k_prev;
+    grad_x_k = gradient(M, b, m, x_k);
+    v_k = v_k_prev - (a_k / L) * grad_x_k;
+    
+    if evaluate_func(M, b, m, y_k_prev - gradient(M, b, m, y_k_prev) / L) < evaluate_func(M, b, m, x_k - grad_x_k / L)
+        y_k = y_k_prev - gradient(M, b, m, y_k_prev) / L;
+    else
+        y_k = x_k - grad_x_k / L;
+    end
 end
 
 function nesterovForSmoothStronglyConvex()
@@ -158,14 +151,27 @@ function nesterovForSmoothStronglyConvex()
     end
 end
 
-function [x_k, p_k] = conjugateGradientMethod(M, b, x_k_prev, p_k_prev)
-    gradient_x_k_prev = gradient(M, b, x_k_prev);
+function [y_k, v_k, A_k] = nesterovForSmoothStronglyConvexRechtMethod(y_k_prev, v_k_prev, M, b, L, m, A_k_prev)
+    m0 = L - m;
+    a_k = ((m0 + 2*m*A_k_prev) + sqrt((m0 + 2*m*A_k_prev)^2 + 4*m0*(m0*A_k_prev + m*A_k_prev^2))) / 2*m0;
+    A_k = A_k_prev + a_k;
+    
+    a_k_ = a_k * (m0 + m*A_k_prev) / (m0 + m*A_k);
+    theta_k = a_k_ / (A_k_prev + a_k_);
+    
+    x_k = (1 - theta_k)*y_k_prev + theta_k*v_k_prev;
+    v_k = (m0 + m*A_k_prev) / (m0 + m*A_k) * v_k_prev + (m*a_k) / (m0 + m*A_k) * x_k - a_k / (m0 + m*A_k) * gradient(M, b, m, x_k);
+    y_k = x_k - 1 / L * gradient(M, b, m, x_k);
+end
+
+function [x_k, p_k] = conjugateGradientMethod(M, b, m, x_k_prev, p_k_prev)
+    gradient_x_k_prev = gradient(M, b, m, x_k_prev);
     
     % Exact line search for h_k_prev
     h_k_prev = - dot(gradient_x_k_prev, -p_k_prev) / dot(-M*p_k_prev, -p_k_prev);
     x_k = x_k_prev - h_k_prev * p_k_prev;
     
-    beta_k_prev = dot(gradient_x_k_prev, gradient_x_k_prev) / dot(gradient(M, b, x_k) - gradient_x_k_prev, p_k_prev);
+    beta_k_prev = dot(gradient_x_k_prev, gradient_x_k_prev) / dot(gradient(M, b, m, x_k) - gradient_x_k_prev, p_k_prev);
     p_k = gradient_x_k_prev - beta_k_prev * p_k_prev;
 end
 
@@ -173,7 +179,7 @@ function x_k = heavyBallMethod(x_k_prev, x_k_prev_prev, M, b, L, m)
     alpha_1 = 4 / (sqrt(L) + sqrt(m))^2;
     alpha_2 = (sqrt(L) - sqrt(m))^2 / (sqrt(L) + sqrt(m))^2;
     
-    x_k = x_k_prev - alpha_1 * gradient(M, b, x_k_prev) + alpha_2 * (x_k_prev - x_k_prev_prev);
+    x_k = x_k_prev - alpha_1 * gradient(M, b, m, x_k_prev) + alpha_2 * (x_k_prev - x_k_prev_prev);
 end
 
 % Helper method to evaluate the value of function at a given input
