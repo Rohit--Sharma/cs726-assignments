43921976722cc49c44d78ceb2c3aed4538ff7f61
Rohit <rohit.sharma@wisc.edu>
2020-04-13 22:31:51 -0500

    Changes to q1 and q3

--- assignments/assignment5/q1.m
+++ assignments/assignment5/q1.m
@@ -3,19 +3,72 @@ function q1(n, n_iter)
     rng('default') % For reproducibility
     p = 0.5;
     Z = 2*binornd(1, p * ones(n)) - 1;
+    M1 = 1;
+    M2 = sqrt(n);
     
-    x_k = 1/n * ones(n, 1);
-    L=1;
+    x_k_pgd = 1/n * ones(n, 1);
+    x_k_pgd_out = x_k_pgd;
+    A_k_pgd = 1;
+    f_pgd_iterate_vals = zeros(n_iter, 1);
+    f_pgd_out_vals = zeros(n_iter, 1);
+    
+    x_k_md = 1/n * ones(n, 1);
+    x_k_md_out = x_k_md;
+    A_k_md = 1;
+    f_md_iterate_vals = zeros(n_iter, 1);
+    f_md_out_vals = zeros(n_iter, 1);
     
     for iter = 1 : n_iter
-        x_k = ProjectedGradientDescent(x_k, Z, L)
-        f_val = max(x_k' * Z)
+        [x_k_pgd, x_k_pgd_out, A_k_pgd] = ProjectedGradientDescent(x_k_pgd, x_k_pgd_out, Z, M2, iter, A_k_pgd);
+        f_pgd_iterate_vals(iter) = max(x_k_pgd' * Z);
+        f_pgd_out_vals(iter) = max(x_k_pgd_out' * Z);
+        
+        [x_k_md, x_k_md_out, A_k_md] = MirrorDescent(x_k_md, x_k_md_out, M1, iter, A_k_md);
+        f_md_iterate_vals(iter) = max(x_k_md' * Z);
+        f_md_out_vals(iter) = max(x_k_md_out' * Z);
     end
+    
+    disp(f_md_out_vals);
+    
+    % Plot part (i): Optimality gap for SD:Const, SD:Exact and Nesterov
+    figure
+    plot(1:1:n_iter, f_pgd_iterate_vals)
+    set(gca, 'YScale', 'log')
+    hold on
+    plot(1:1:n_iter, f_md_iterate_vals)
+    legend('PGD', 'Mirror Descent')
+    title('Analysis of Optimization algorithms')
+    xlabel('Num iterations')
+    ylabel('f(x_k)');
+    
+    % Plot part (i): Optimality gap for SD:Const, SD:Exact and Nesterov
+    figure
+    plot(1:1:n_iter, f_pgd_out_vals)
+    set(gca, 'YScale', 'log')
+    hold on
+    plot(1:1:n_iter, f_md_out_vals)
+    legend('PGD', 'Mirror Descent')
+    title('Analysis of Optimization algorithms')
+    xlabel('Num iterations')
+    ylabel('f(x_k^{out})');
 end
 
-function x_k = ProjectedGradientDescent(x_k_prev, Z, L)
-    x_k_prev = x_k_prev - 1/L * grad(x_k_prev, Z);
+function [x_k, x_k_out, A_k] = ProjectedGradientDescent(x_k_prev, x_k_out_prev, Z, M, k, A_k_prev)
+    a_k = 1 / (M * sqrt(k));
+    A_k = A_k_prev + a_k;
+    
+    x_k_prev = x_k_prev - a_k * grad(x_k_prev, Z);
     x_k = ProjectOntoSimplex(x_k_prev', 1)';
+    x_k_out = 1/A_k * (a_k*x_k + A_k_prev*x_k_out_prev);
+end
+
+function [x_k, x_k_out, A_k] = MirrorDescent(x_k_prev, x_k_out_prev, M, k, A_k_prev)
+    a_k = 1 / (M * sqrt(k));
+    A_k = A_k_prev + a_k;
+    
+    v_k = a_k + 1 - log(x_k_prev);
+    x_k = exp(v_k) / sum(exp(v_k));
+    x_k_out = 1/A_k * (a_k*x_k + A_k_prev*x_k_out_prev);
 end
 
 function subgrad = grad(x, Z)
--- assignments/assignment5/q3.m
+++ assignments/assignment5/q3.m
@@ -19,20 +19,52 @@ function [outputArg1,outputArg2] = q3(n, n_iter, epsilon)
     nesterov_y_k = nesterov_v_k;
     nesterov_opt_gap = [];
     
+    % SD:Const initializations
+    sdconst_x_k = zeros(n, 1);
+    sdconst_opt_gap = [];
+    
+    % SGD initializations:
+    sgd_x_k = zeros(n, 1);
+    sgd_x_k_out = sgd_x_k;
+    sgd_A_k = 1/L;
+    sgd_opt_gap = [];
+    
     % Optimization loop
     for k = 1 : n_iter
         % Run Nesterov's acceleration step
         [nesterov_y_k, nesterov_v_k, nesterov_A_k] = nesterovsMethod(nesterov_y_k, nesterov_v_k, M, b, L, nesterov_A_k, epsilon);
         nesterov_f = evaluate_func(M, b, nesterov_y_k);
         nesterov_opt_gap = [nesterov_opt_gap, nesterov_f - f_optimal];
+        
+        % Run SD:Const step
+        sdconst_x_k = steepestDescent(sdconst_x_k, M, b, L, epsilon);
+        steep_desc_f = evaluate_func(M, b, sdconst_x_k);
+        sdconst_opt_gap = [sdconst_opt_gap, steep_desc_f - f_optimal];
+        
+        [sgd_x_k_out, sgd_x_k, sgd_A_k] = projectedSGDWithAvg(sgd_x_k_out, sgd_x_k, M, b, L, k, sgd_A_k, epsilon);
+        sgd_f = evaluate_func(M, b, sgd_x_k_out);
+        sgd_opt_gap = [sgd_opt_gap, sgd_f - f_optimal];
     end
     
-    disp(nesterov_opt_gap);
+    % disp(nesterov_opt_gap);
+    
+    % Plot part (i): Optimality gap for SD:Const, SD:Exact and Nesterov
+    figure
+    plot(1:1:n_iter, sdconst_opt_gap)
+    set(gca, 'YScale', 'log')
+    hold on
+    plot(1:1:n_iter, nesterov_opt_gap)
+    hold on
+    plot(1:1:n_iter, sgd_opt_gap)
+    legend('SD:constant', 'Nesterov', 'SGD')
+    title(strcat('Analysis of Optimization algorithms, eps=', num2str(epsilon)))
+    xlabel('Num iterations')
+    ylabel('Optimality gap: f(x) - f(x*)');
 end
 
 % Run one step of steepest descent with constant step size = 1/L
-function x_k = steepestDescent(x_k_prev, M, b, L)
-    x_k = x_k_prev - (1 / L) * gradient(M, b, x_k_prev);
+function x_k = steepestDescent(x_k_prev, M, b, L, epsilon)
+    x_k = x_k_prev - (1 / L) * gradient(M, b, epsilon, x_k_prev);
 end
 
 % Run one step of Nesterov's acceleration for smooth f
@@ -45,6 +77,14 @@ function [y_k, v_k, A_k] = nesterovsMethod(y_k_prev, v_k_prev, M, b, L, A_k_prev
     grad_x_k = gradient(M, b, epsilon, x_k);
     v_k = v_k_prev - (a_k / L) * grad_x_k;
     y_k = x_k - grad_x_k / L;
+end
+
+function [x_k_out, x_k, A_k] = projectedSGDWithAvg(x_k_out_prev, x_k_prev, M, b, L, k_prev, A_k_prev, epsilon)
+    a_k = 1 / (L*sqrt(k_prev+1));
+    A_k = A_k_prev + a_k;
+    
+    x_k = x_k_prev - a_k * gradient(M, b, epsilon, x_k_prev);
+    x_k_out = 1/A_k * (a_k*x_k + A_k_prev*x_k_out_prev);
 end
 
 % Initialize M(nxn) and b(nx1) as required
